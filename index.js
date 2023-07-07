c2a549097bbf9df2d4b94a54bef4ca50b3d9fb77
SeemaMistry <seema.m.1996@gmail.com>
2023-07-07 18:19:59 -0400

    feat: add express handlebar and set middleware route

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const exphbs = require('express-handlebars');
+
+// set handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// set handlebar routes
+app.get('/', function (req, res) {
+  res.render('home');
+});
 
 // set port to system's or 5000 default
 const PORT = process.env.PORT || 5000;
