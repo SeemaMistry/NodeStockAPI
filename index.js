7a75fc077d80fc214ca43b3d74ec8006c5e2e9e8
SeemaMistry <seema.m.1996@gmail.com>
2023-07-07 18:09:30 -0400

    feat: add static file route to index.html

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // set port to system's or 5000 default
 const PORT = process.env.PORT || 5000;
+
+// set routes
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log('port is working on: ' + PORT));
