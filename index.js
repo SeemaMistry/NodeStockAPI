050a6ce4964d00c11c4b97009dca713e9cd1c7ae
SeemaMistry <seema.m.1996@gmail.com>
2023-07-08 01:37:13 -0400

    feat: add express-handlebars middleware to render template views

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const app = express();
+const { engine } = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 const path = require('path');
-const exphbs = require('express-handlebars');
+
+// set port to system's or 5000 default
+const PORT = process.env.PORT || 5000;
 
 // set handlebars middleware
-app.engine('handlebars', exphbs());
+app.engine(
+  'handlebars',
+  engine({ extname: '.handlebars', defaultLayout: 'main' })
+);
 app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 // set handlebar routes
 app.get('/', function (req, res) {
   res.render('home');
 });
-
-// set port to system's or 5000 default
-const PORT = process.env.PORT || 5000;
 
 // set routes
 app.use(express.static(path.join(__dirname, 'public')));
