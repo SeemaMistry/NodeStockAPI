3ddb4914377d19a63f54bd16f484b7f1593da5b8
SeemaMistry <seema.m.1996@gmail.com>
2023-07-09 02:05:18 -0400

    feat: get LookUp bar searhTerm value inserted into Alpha Vantage API working

--- index.js
+++ index.js
@@ -23,39 +23,86 @@ app.set('views', './views');
 app.use(express.json())
 app.use(express.urlencoded());
 
+const content = { error: "",  stuff: "I am displaying some stuff", content: "200 okay",
+            obj: [1,2,3], obj2: [{id:"hi", "name. 1":"world"}, {id:"bye", "name. 1":"sky"}]
+
+}
+// Alpha Vantage API
+var request = require('request');
+let url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${content.searchTerm}&apikey=7NRVXF6FRD2KZPF3`;
+
+request.get({
+  url: url,
+  json: true,
+  headers: {'User-Agent': 'request'}
+}, (err, res, data) => {
+  if (err) {
+    content['error'] = "There were no stocks that matched.\nUse the LookUp search in the top right corner to search up a ticker."
+
+    console.log('Error:', err);
+    console.log(content.error)
+
+  } else if (res.statusCode !== 200) {
+    content.error = "There were no stocks that matched.\nUse the LookUp search in the top right corner to search up a ticker."
+    console.log(content.error)
+    console.log('Status:', res.statusCode);
+  } else {
+    // data is successfully parsed as a JSON object:
+    console.log(data);
+
+    if (data.bestMatches.length === 0) {
+      content.error = "There were no stocks that matched.\nUse the LookUp search in the top right corner to search up a ticker."
+    }
+    content.error = ""
+
+    // add data to content{}
+    content.stocks = data.bestMatches;
+    
+    // add only the ticker symbol to the stock{}
+    data.bestMatches.forEach(stock => {
+      console.log(stock['1. symbol'])
+    })
+
+
+  }
+});
+
+
+
 // set handlebar routes
 app.get('/', function (req, res) {
   res.render('home', content );
 });
 
+app.post('/', function (req, res) {
+  content.searchTerm = req.body.searchTerm;
+  console.log(req.body.searchTerm)
+  url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${req.body.searchTerm}&apikey=7NRVXF6FRD2KZPF3`;
+  console.log(url)
 
-app.post('/about', function (req, res) {
- res.render('about', {searchTerm: req.body.searchTerm} );
-});
 
-// Alpha Vantage API
-var request = require('request');
-
-// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-var url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=microsoft&apikey=7NRVXF6FRD2KZPF3';
-
-const content = { stuff: "I am displaying some stuff", content: "200 okay",
-            obj: [1,2,3], obj2: [{id:"hi", "name. 1":"world"}, {id:"bye", "name. 1":"sky"}]
-
-}
-
-request.get({
+  request.get({
     url: url,
     json: true,
     headers: {'User-Agent': 'request'}
   }, (err, res, data) => {
     if (err) {
+      content['error'] = "There were no stocks that matched.\nUse the LookUp search in the top right corner to search up a ticker."
+
       console.log('Error:', err);
+      console.log(content.error)
+
     } else if (res.statusCode !== 200) {
+      content.error = "There were no stocks that matched.\nUse the LookUp search in the top right corner to search up a ticker."
+      console.log(content.error)
       console.log('Status:', res.statusCode);
     } else {
       // data is successfully parsed as a JSON object:
       console.log(data);
+
+      if (data.bestMatches.length === 0) {
+        content.error = "There were no stocks that matched.\nUse the LookUp search in the top right corner to search up a ticker."
+      }
       
       // add data to content{}
       content.stocks = data.bestMatches;
@@ -68,6 +115,16 @@ request.get({
 
     }
 });
+
+ res.render('home', content );
+});
+
+
+
+// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+// var url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=microsoft&apikey=7NRVXF6FRD2KZPF3`;
+
+
 
 
 
